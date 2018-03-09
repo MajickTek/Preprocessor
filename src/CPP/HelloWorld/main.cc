1d43314017a5b4283f5258e4aee198408c5b83a4
Majick Tek <kingtmw2@gmail.com>
2018-03-09 15:43:45 -0500

    Create main.cc

---
+++ src/CPP/HelloWorld/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    #include "cout.cc" // std::cout
+    #include "pipe.cc" // <<
+    #include "helloworld.cc" // "hello, world
+    #include "pipe.cc" // << (you can include it twice)
+    #include "endl.cc" // std::endl;
+    #include "semicolon.cc" // ;
+}
