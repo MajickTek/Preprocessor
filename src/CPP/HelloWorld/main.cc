f9783477a3a5c02813fa3a9b99b62b50cbbfe5e7
Majick Tek <kingtmw2@gmail.com>
2018-03-09 15:49:03 -0500

    Update main.cc

--- src/CPP/HelloWorld/main.cc
+++ src/CPP/HelloWorld/main.cc
@@ -1,3 +1,9 @@
+/*
+This is all legal because the preprocessor/compiler combines all the headers into one file.
+This means that (during compilation) this code will become:
+std::cout << "Hello, world!" << std::endl;
+*/
+
 #include <iostream>
 
 int main() {
